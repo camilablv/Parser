98b9ca30518d2e8d591746e2e35e3ce32138eaca
Camila-1 <abalova.kamila@mail.ru>
2018-12-05 10:18:14 +0500

    добавлены файлы zor.h и zor.cpp

--- Parser/zor.cpp
+++ Parser/zor.cpp
@@ -4,3 +4,8 @@ Zor::Zor(QObject *parent) : QObject(parent)
 {
 
 }
+Zor::~Zor()
+{
+
+}
+
--- Parser/zor.h
+++ Parser/zor.h
@@ -2,16 +2,26 @@
 #define ZOR_H
 
 #include <QObject>
+#include <QUrl>
+#include <qgumbodocument.h>
+#include <qgumbonode.h>
+#include <qgumboattribute.h>
+#include <write.h>
 
 class Zor : public QObject
 {
     Q_OBJECT
 public:
     explicit Zor(QObject *parent = nullptr);
+    ~Zor();
+    void Start();
 
 signals:
 
 public slots:
+
+private:
+
 };
 
 #endif // ZOR_H
